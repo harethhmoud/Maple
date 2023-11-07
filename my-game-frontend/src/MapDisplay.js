c8a32cd178d9cc2312df2827cac7d98652a457ce
harethhmoud <hareth.hmoud@gmail.com>
2023-11-07 13:50:46 -0500

    Created displays.

---
+++ my-game-frontend/src/MapDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MapDisplay() {
+  return (
+    <div className = "map-display">
+        <img src ="tbd" alt="Map of the day"/>
+    </div>
+  )
+}
+
+export default MapDisplay;
+
